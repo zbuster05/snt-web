bb8949c5cbfc7601bb58dc0c7e9c1cf02ee0f61b
zbuster05 <zakary.saya@gmail.com>
2021-11-29 10:59:35 -0800

    SNT web

---
+++ precache-manifest.3994f2df636d618f03f57c6883f213ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afed8eae890acbeb32eacb09c81660ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d9b0085008b8ca5b82",
+    "url": "/static/css/main.c52e4148.chunk.css"
+  },
+  {
+    "revision": "5434d9bad5e9f0108e54",
+    "url": "/static/js/2.f225d30b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f225d30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d9b0085008b8ca5b82",
+    "url": "/static/js/main.5f27ac23.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  }
+]);
